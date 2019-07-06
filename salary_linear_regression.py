2b05a34efd8ef9270983b7a597962a71c8ec4498
Parakh Gupta <parakhgupta.98@gmail.com>
2019-07-06 20:19:03 +0530

    Add files

---
+++ salary_linear_regression.py
@@ -0,0 +1,41 @@
+# Simple Linear Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+# Fitting Simple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = reg.predict(X_test)
+
+# Visualising the Training set results
+plt.scatter(X_train, y_train, color='green')
+plt.plot(X_train, reg.predict(X_train), color='blue')
+plt.title('Salary vs Experience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+plt.savefig('train.jpg')
+
+# Visualising the Test set results
+plt.scatter(X_test, y_test, color='red')
+plt.plot(X_train, reg.predict(X_train), color='blue')
+plt.title('Salary vs Experience (Test set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+plt.savefig('test.jpg')
